c4cca6b42ad27417683dc57352f5c85634a9bbf7
Dwight Newton <38666846+DwightNewton@users.noreply.github.com>
2021-04-30 15:34:24 -0400

    Add files via upload

---
+++ Differential_WGCNA_Permutations_SCZ_parallelization.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+#SBATCH --time=12:00:00
+#SBATCH --array=1-1000
+#SBATCH --ntasks=1
+#SBATCH --tasks-per-node=1
+#SBATCH --mem-per-cpu=6144M
+#SBATCH --cpus-per-task=1
+#SBATCH --mail-type=END
+
+sed "${SLURM_ARRAY_TASK_ID}q;d" Control_vs_SCHIZ_permutations_density_threshold.cmdlist | bash
